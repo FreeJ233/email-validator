7e31ce59f04145aaf6f78b5f057d786721ffaa95
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2024-12-04 15:24:26 +0800

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+{
+  "name": "email-validator-backend",
+  "version": "1.0.0",
+  "description": "Backend for email validation",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.17.3",
+    "email-verifier": "^1.0.1"
+  }
+}
